869734ae12e15424492d74aa86e291f62d209af2
Kevin <Kevinpede98@gmail.com>
2022-05-31 17:23:21 +0200

    FAQ

---
+++ faqscript.js
@@ -0,0 +1,15 @@
+var acc = document.getElementsByClassName("spørgsmål");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    this.classList.toggle("aktiv");
+
+    var svar = this.nextElementSibling;
+    if (svar.style.display === "block") {
+        svar.style.display = "none";
+        } else {
+            svar.style.display = "block";
+        }
+    });
+}
