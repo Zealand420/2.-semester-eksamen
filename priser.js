af2e548665d6c8fe266e02566547f2fa3e07d446
ibenbausager <iben4545@edu.zealand.dk>
2022-06-05 21:09:58 +0200

    kommentar

--- priser.js
+++ priser.js
@@ -4,7 +4,10 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
     this.classList.toggle("aktiv");
+    /*Skift mellem at tilføje og fjerne den "aktive" class,
+    for at fremhæve den knap, der styrer svar*/ 
 
+    /*Skift mellem at skjule og vise det aktive svar */
     var svar = this.nextElementSibling;
     if (svar.style.display === "block") {
          svar.style.display = "none";
