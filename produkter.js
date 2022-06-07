023504e4d38786f5c33581f36c7cb27817f6ab07
LaerkeSoeby <laer9384@edu.zealand.dk>
2022-06-07 09:37:25 +0200

    Rettelse

--- produkter.js
+++ produkter.js
@@ -21,7 +21,7 @@ function bestilling2() {
     var email = 'laer9384@edu.zealand.dk';
     var subject = 'Bestilling - Collagile Dog';
     var emailBody = "Hej. %0D%0A" +
-        "Jeg ønsker at bestille følgende vare: Collagile Dog%0D%0" +
+        "Jeg ønsker at bestille følgende vare: Collagile Dog%0D%0A" +
         "Antal:%0D%0A" +
         "Kontakt mig via (tlf el. email):%0D%0A" +
         "Hilsen:%0D%0A";
